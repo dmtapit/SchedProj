721abf645774c7cac1d743af683cc1cdf26180b7
Dean Tapit <dmtapit@gmail.com>
2018-03-21 18:42:25 -0700

    Add basic features of a frame directly into main.h, main.cpp

--- SchedProj/main.cpp
+++ SchedProj/main.cpp
@@ -1,12 +1,66 @@
 #include "main.h"
-#include "CellSheet.h"
+//#include "CellSheet.h"
 
 IMPLEMENT_APP(SchedProjApp)
 
 bool SchedProjApp::OnInit()
 {
-	CellSheet *cs = new CellSheet(wxT("CellSheet"));
-	cs->Show(true);
+	//CellSheet *cs = new CellSheet(wxT("CellSheet"));
+	//cs->Show(true);
 
+	BasicFrame *frame = new BasicFrame(wxT("Basic"), 50, 50, 450, 300);
+
+	frame->Show(true);
+	SetTopWindow(frame);
 	return true;
+}
+
+BasicFrame::BasicFrame(const wxChar *title, int xpos, int ypos, int width, int height)
+	: wxFrame((wxFrame*)NULL, -1, title, wxPoint(xpos, ypos), wxSize(width, height))
+{
+	// Pointer safety.  Always initialize any pointer you have declared in an interface file.
+	theText = (wxTextCtrl *)NULL;
+	menuBar = (wxMenuBar *)NULL;
+	fileMenu = (wxMenu *)NULL;
+	
+	theText = new wxTextCtrl(this, -1, wxString("stringstringstrginstring\n\n"
+		"More string."), wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE);
+	
+	fileMenu = new wxMenu;
+	fileMenu->Append(BASIC_OPEN, wxT("&Open File"), wxT("Open an existing file"));
+	fileMenu->Append(BASIC_ABOUT, wxT("&About"), wxT("Who wrote this!"));
+	fileMenu->AppendSeparator();
+	fileMenu->Append(BASIC_EXIT, wxT("E&xit"), wxT("Stop wasting time."));
+
+	menuBar = new wxMenuBar;
+	menuBar->Append(fileMenu, wxT("&File"));
+	SetMenuBar(menuBar);
+	CreateStatusBar(3);
+}
+
+BasicFrame::~BasicFrame() {}
+
+BEGIN_EVENT_TABLE (BasicFrame, wxFrame)
+	EVT_MENU (BASIC_OPEN, BasicFrame::OnOpenFile)
+	EVT_MENU (BASIC_ABOUT, BasicFrame::OnAbout)
+	EVT_MENU (BASIC_EXIT, BasicFrame::OnExit)
+END_EVENT_TABLE()
+
+void BasicFrame::OnOpenFile(wxCommandEvent & event)
+{
+	theText->LoadFile(wxT("data.txt"));
+}
+
+void BasicFrame::OnAbout(wxCommandEvent & event)
+{
+	wxString t = TITLE; //TITLE from main.h
+	t.append(wxT("\nDB 2001"));
+
+	wxMessageDialog aboutDialog(this, t, wxT("About Basic"), wxOK | wxCANCEL);
+	aboutDialog.ShowModal();
+}
+
+void BasicFrame::OnExit(wxCommandEvent & event)
+{
+	Close(true);
 }
--- SchedProj/main.h
+++ SchedProj/main.h
@@ -1,8 +1,33 @@
 #pragma once
 #include <wx/wx.h>
 
+static const wxChar *TITLE = wxT("Title of the App");
+
 class SchedProjApp : public wxApp
 {
 public:
-	virtual bool OnInit();
+	virtual bool OnInit(); // Overriding the OnInit() method, thus declare it virtual
+};
+
+class BasicFrame : public wxFrame
+{
+public:
+	BasicFrame(const wxChar *title, int xpos, int ypos, int width, int height);
+	~BasicFrame();
+
+	wxTextCtrl	*theText;
+	wxMenuBar *menuBar;
+	wxMenu *fileMenu;
+
+	void OnOpenFile(wxCommandEvent & event);
+	void OnAbout(wxCommandEvent & event);
+	void OnExit(wxCommandEvent & event);
+
+	DECLARE_EVENT_TABLE() // Alert the compiler that this class will have an event table
+};
+
+enum {
+	BASIC_EXIT = 1,
+	BASIC_OPEN = 100,
+	BASIC_ABOUT = 200
 };
