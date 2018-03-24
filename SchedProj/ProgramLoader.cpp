91ec6c6a60380ad7744a71dc485a87528c8c8144
Dean Tapit <dmtapit@gmail.com>
2018-03-24 13:30:55 -0700

    Create a program loader; button that loads Basic Application frame

--- SchedProj/ProgramLoader.cpp
+++ SchedProj/ProgramLoader.cpp
@@ -1,4 +1,5 @@
 #include "ProgramLoader.h"
+#include "BasicFrame.h"
 
 ProgramLoader::ProgramLoader(const wxChar *title, int xpos, int ypos, int width, int height)
 	: wxFrame((wxFrame*)NULL, -1, title, wxPoint(xpos, ypos), wxSize(width, height))
@@ -14,6 +15,17 @@ ProgramLoader::ProgramLoader(const wxChar *title, int xpos, int ypos, int width,
 	SetMenuBar(menuBar);
 	CreateStatusBar(2);
 
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	wxPanel *panel = new wxPanel(this, -1);
+	
+	wxBoxSizer *hbox = new wxBoxSizer(wxHORIZONTAL);
+	
+	btnPanel = new ButtonPanel(panel);
+	hbox->Add(btnPanel, 2, wxEXPAND | wxRIGHT, 10);
+
+	panel->SetSizer(hbox);
+
 	Center();
 }
 
@@ -23,5 +35,29 @@ END_EVENT_TABLE()
 
 void ProgramLoader::OnLoadProgram(wxCommandEvent & event)
 {
+	
+	
+}
 
+ButtonPanel::ButtonPanel(wxPanel * parent) : wxPanel(parent, wxID_ANY)
+{
+	wxBoxSizer *vbox = new wxBoxSizer(wxVERTICAL);
+
+	btn_Basic = new wxButton(this, BASIC_APPLICATION, wxT("Basic Application"));
+
+	Connect(BASIC_APPLICATION, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(ButtonPanel::OnBasicDialog));
+
+	vbox->Add(btn_Basic);
+
+	SetSizer(vbox);
+}
+
+
+/*
+	When the Basic Application button is clicked, it opens a new window (program) of BasicFrame
+*/
+void ButtonPanel::OnBasicDialog(wxCommandEvent& event)
+{
+	BasicFrame *frame = new BasicFrame(wxT("BASIC APP"), 50, 50, 450, 300);
+	frame->Show(true);
 }
--- SchedProj/ProgramLoader.h
+++ SchedProj/ProgramLoader.h
@@ -1,6 +1,20 @@
 #pragma once
 #include <wx/wx.h>
 
+class ButtonPanel : public wxPanel
+{
+public:
+	ButtonPanel(wxPanel *parent);
+
+	void OnBasicDialog(wxCommandEvent& event);
+
+	wxButton *btn_Basic;
+};
+
+enum {
+	BASIC_APPLICATION = wxID_HIGHEST + 1
+};
+
 class ProgramLoader : public wxFrame
 {
 public:
@@ -9,7 +23,9 @@ public:
 	wxMenuBar *menuBar;
 	wxMenu *fileMenu;
 
-	void OnLoadProgram(wxCommandEvent & event);
+	void OnLoadProgram(wxCommandEvent& event);
+
+	ButtonPanel *btnPanel;
 
 	DECLARE_EVENT_TABLE()
 };
