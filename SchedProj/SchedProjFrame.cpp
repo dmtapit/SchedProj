62ca251bb47054573dfd266e7d763adfaa1975ef
Dean Tapit <dmtapit@gmail.com>
2018-04-02 00:59:31 -0700

    Flesh out the wxPanel and its contents to be used in the SchedProjFrame

--- SchedProj/SchedProjFrame.cpp
+++ SchedProj/SchedProjFrame.cpp
@@ -2,7 +2,7 @@
 // Program:		SchedProj
 // Name:		SchedProjFrame.cpp
 // Author:		Dean Tapit
-// Last Edit:	3/31/18
+// Last Edit:	4/1/18
 // Info:		Based off some wxWidgets tutorials on the wiki
 //			This is the MAIN FRAME code for Sched Project.
 //			Based off of minimal.cpp sample code.
@@ -13,15 +13,32 @@
 
 #include "SoulPage.h"
 
-//////////////////////////
-// MAIN FRAME
-//////////////////////////
+///////////////////////////////////////////////////////////////////////
+// Event tables and other macros for wxWidgets
+///////////////////////////////////////////////////////////////////////
+wxBEGIN_EVENT_TABLE(SchedProjFrame, wxFrame)
+EVT_MENU(Minimal_Quit, SchedProjFrame::OnQuit)
+EVT_MENU(Minimal_About, SchedProjFrame::OnAbout)
 
-// frame constructor
+EVT_MENU(ON_NEW_LIST, SchedProjFrame::OnNewList)
+wxEND_EVENT_TABLE()
+
+///////////////////////////////////////////////////////////////////////
+//
+// MAIN FRAME Construction
+//
+///////////////////////////////////////////////////////////////////////
+
 SchedProjFrame::SchedProjFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(600, 500))
 {
-#if wxUSE_MENUS
+
+	// set the frame icon
+	///	SetIcon(wxICON(sample));
+
+#if wxUSE_MENUS // [Dean Tapit: just realized how awesome these if statements are: they are basically options]
 	// create a menu bar
+	wxMenuBar *menuBar = new wxMenuBar();
+
 	wxMenu *fileMenu = new wxMenu;
 	fileMenu->Append(ON_NEW_LIST, "&New List\tAlt-N", "Create a new list");
 	fileMenu->AppendSeparator();
@@ -30,7 +47,7 @@ SchedProjFrame::SchedProjFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, 
 	wxMenu *helpMenu = new wxMenu;
 	helpMenu->Append(Minimal_About, "&About\tF1", "Show about dialog");
 	
-	wxMenuBar *menuBar = new wxMenuBar();
+	// Appending the menus to the menuBar
 	menuBar->Append(fileMenu, "&File");
 	menuBar->Append(helpMenu, "&Help");
 
@@ -44,6 +61,35 @@ SchedProjFrame::SchedProjFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, 
 	sizer->Add(aboutBtn, wxSizerFlags().Center());
 #endif // wxUSE_MENUS/!wxUSE_MENUS
 
+	// create controls
+	m_panel = new wxPanel(this, wxID_ANY);
+	wxSizer *sizerTop = new wxBoxSizer(wxVERTICAL);
+
+	// The pane should contain the widget (notebook) to be used.  Based on the original code
+	// from "widgets.cpp", the two panes will kept as is for my own learning purposes
+	// Can add the rest of the log window code if I want to later
+
+	int style = wxBK_DEFAULT;
+	// Uncomment to suppress page theme (draw in solid colour)
+	//style |= wxNB_NOPAGETHEME;
+
+	m_book = new SchedProjBookCtrl(m_panel, SchedProj_BookCtrl,
+		wxDefaultPosition, wxDefaultSize, style, "SchedProj - Book Ctrl");
+
+	InitBook(); // CODE UNDER CONSTRUCTION
+
+	// Code for the second pane (lower/down) starts here
+	// Unlike the "widgets.cpp", no log window will be added for now, just get the notebook going
+	// To keep the code from getting too much and hinder my understanding of it, the log portion
+	// is kept out. Can add it back in when I think I have gotten the gist of this whole thing...
+	// see line 486, "widgets.cpp"
+	wxSizer *sizerDown = new wxBoxSizer(wxVERTICAL);
+
+	wxBoxSizer *sizerBtns = new wxBoxSizer(wxHORIZONTAL);
+	wxButton *btn;
+
+	btn = new wxButton(m_panel, SchedProj_Quit, wxT("Exit"));
+
 #if wxUSE_STATUSBAR
 	CreateStatusBar(2);
 	SetStatusText("Sched Proj App: Welcome!");
@@ -52,19 +98,25 @@ SchedProjFrame::SchedProjFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, 
 	Center(); // Centers the window frame to the center of the screen/monitor
 }
 
-// Event tables and other macros for wxWidgets
-wxBEGIN_EVENT_TABLE(SchedProjFrame, wxFrame)
-	EVT_MENU(Minimal_Quit, SchedProjFrame::OnQuit)
-	EVT_MENU(Minimal_About, SchedProjFrame::OnAbout)
+void SchedProjFrame::InitBook()
+{
+	// TODO
+}
 
-	EVT_MENU(ON_NEW_LIST, SchedProjFrame::OnNewList)
-wxEND_EVENT_TABLE()
+SchedProjFrame::~SchedProjFrame()
+{
+	// NOTE: The code in SchedProjFrame currently does not utilize a Log window 
+	// unlike in the sample code in "widgets.cpp". This code was the only one in it.
+#if USE_LOG
+	delete m_logTarget;
+#endif //USE_LOG
+}
 
-
-
-//////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+//
 // Event Handlers
-//////////////////////////////
+//
+/////////////////////////////////////////////////////////////////////////////////////////
 
 void SchedProjFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
 {
--- SchedProj/SchedProjFrame.h
+++ SchedProj/SchedProjFrame.h
@@ -2,14 +2,49 @@
 // Program:		SchedProj
 // Name:		SchedProjFrame.h
 // Author:		Dean Tapit
-// Last Edit:	3/31/18
+// Last Edit:	4/1/18
 // Info:		Based off some wxWidgets tutorials on the wiki
 //			This is the MAIN FRAME code for Sched Project.
-//			Based off of minimal.cpp sample code.
+//			Based off of "minimal.cpp" sample code.
+//			Some more code based from "widgets.h"
 ///////////////////////////////////////////////////////////////////////
 
 #pragma once
 #include <wx/wx.h>
+
+#if wxUSE_TREEBOOK
+#include "wx/treebook.h"
+#define USE_TREEBOOK 1
+#define SchedProjBookCtrl wxTreebook
+#endif // wxUSE_TREEBOOK
+
+//////////////////////////////////////////////////////////////////
+// ID's for controls and menu commands
+//////////////////////////////////////////////////////////////////
+enum
+{
+	// control ids /////////////////////////////////////////////////////////////
+
+	SchedProj_Quit, // For the quit button in SchedProjFrame.cpp, lower panel
+	SchedProj_BookCtrl,
+
+	// menu items //////////////////////////////////////////////////////////////
+	Minimal_Quit = wxID_EXIT,
+
+	// it is important for the id corresponding to the "About" command to have
+	// this standard value as otherwise it won't be handled properly under Mac
+	// (where it is special and put into the "Apple" menu)
+	Minimal_About = wxID_ABOUT,
+
+	//------------------------------------------------------------------------
+	ON_NEW_LIST = wxID_HIGHEST + 1
+};
+
+///////////////////////////////////////////////////////////////////////
+//
+// MAIN FRAME (Header file code)
+//
+///////////////////////////////////////////////////////////////////////
 
 class SchedProjFrame : public wxFrame
 {
@@ -21,26 +56,22 @@ public:
 	virtual ~SchedProjFrame();
 
 	// event handlers (these functions should not be virtual)
+
 	void OnQuit(wxCommandEvent& event);
 	void OnAbout(wxCommandEvent& event);
 	void OnNewList(wxCommandEvent& event); // This should aim to create a new list
 
+	// initializing the book: add all pages to it
+	// [Dean Tapit: since this program really only wants to create a notebook, and not pages of widgets, this may change]
+	void InitBook();
+
 private:
+	// the panel containing everything
+	wxPanel *m_panel;
+
+	// the book containing the test pages (the widgets); in SchedProj, just a notebook...?
+	SchedProjBookCtrl *m_book;
+
 	// any class wishing to process wxWidgets events must use this macro
 	wxDECLARE_EVENT_TABLE();
-};
-
-// ID's for controls and menu commands
-enum
-{
-	//menu items
-	Minimal_Quit = wxID_EXIT,
-
-	// it is important for the id corresponding to the "About" command to have
-	// this standard value as otherwise it won't be handled properly under Mac
-	// (where it is special and put into the "Apple" menu)
-	Minimal_About = wxID_ABOUT,
-	
-	//------------------------------------------------------------------------
-	ON_NEW_LIST = wxID_HIGHEST + 1
 };
